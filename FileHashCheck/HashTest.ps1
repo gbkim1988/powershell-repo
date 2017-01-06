c3b6e4917fe9148a51d42226abbf8f27dc745021
gbkim1988 <gbkim1988@gmail.com>
2017-01-06 18:12:45 +0900

    file hash check up

---
+++ FileHashCheck/HashTest.ps1
@@ -0,0 +1,49 @@
+# Powershell 의 자원 소모를 20MB 로 제한
+# sl WSMan:\Localhost\Shell
+# Set-Item .\MaxMemoryPerShellMB 20
+
+# Driver List 호출
+# 아래의 명령으로 드라이버를 출력 시 CD Drive 를 필터하지 못함 
+# Root 칼럼만 추출
+Measure-Command {
+$DriverList = Get-PSDrive -PSProvider "FileSystem" | Where-Object {$_.Used }
+$sha1 = New-Object -TypeName System.Security.Cryptography.sha256cryptoserviceprovider
+
+foreach ( $drive in $DriverList ){
+    Write-Host -f Green "Searching ", $_.Root
+    try{
+        Get-ChildItem $drive.Root -include *.exe,*.dll -r | % { $_.FullName} | foreach {
+            Write-Host -f Red $_ 
+            $filehash = [System.BitConverter]::ToString($sha1.ComputeHash([System.IO.File]::ReadAllBytes($_))) -replace '-',''
+            Write-Host -f Green $filehash
+        } 
+    }catch [PathNotFound,Microsoft.PowerShell.Commands.GetChildItemCommand]{
+        Write-Host "error"
+    }finally{
+        Write-Host "finally"
+    }
+    #$someFilePath = "C:\foo.txt"
+    #
+    #$hash = [System.BitConverter]::ToString($md5.ComputeHash([System.IO.File]::ReadAllBytes($someFilePath)))
+}
+}
+
+# powershell 시간 소요 측정
+# https://www.pluralsight.com/blog/tutorials/measure-powershell-scripts-speed
+
+# 아래의 구문은 Get-WmiObject 명령을 사용하여 드라이브를 불러온다.
+# 아래의 명령은 시스템 예약 영역도 보여주는데 음 .... 의미가 있을지...
+
+#Get-WmiObject Win32_Volume | Format-Table Name, Label, FreeSpace, Capacity
+<#
+Get-WmiObject Win32_Volume -Filter "DriveType='3'" | ForEach {
+    New-Object PSObject -Property @{
+        Name = $_.Name
+        Label = $_.Label
+        FreeSpace_GB = ([Math]::Round($_.FreeSpace /1GB,2))
+        TotalSize_GB = ([Math]::Round($_.Capacity /1GB,2))
+    }
+}#>
+
+# EXE, DLL 의 파일 목록을 호출
+
--- SearchPersonalInformation/GetPersonalInfo.ps1
+++ SearchPersonalInformation/GetPersonalInfo.ps1
@@ -1,34 +1,3 @@
-﻿<# practice001.ps1
-
-Intro.
-YES24.COM 에 근무하고 있는 필자는 어느날 개인정보보호법 컴플라이언스 준수를 위하여 주민등록번호, 카드번호등의 
-정보를 적절한 접근제어가 없이 저장되어 있는 사례를 발견하기 위해 스크립트를 작성하기로 결심하였다. 스크립트를 작성
-하기에 앞서 적절한 스크립트 언어를 선택하여야 하는데 Windows 운영체제를 주로 사용하는 내부 환경을 고려하여 powershell
-스크립트를 사용하기로 결정하였다. 
-
-여기서 문제는 다양한 환경에 대해 powershell 스크립트가 적절히 동작을 수행할 수 있느냐는 점이다. 이점에 대한 조사는 
-추후에 진행하도록 한다. 그 이유는 XP 는 이미 폐기된 OS 이며, 변경을 해야하는 OS 이다. 또한 잔여 비율이 낮으므로 비용대비
-효과가 미비하므로 지속적인 관리보다는 시스템을 개선하는 방향으로 선회하는 것이 옳다고 여겨진다. 
-
-원제로 회귀하여 powershell의 구성방안에 대해 기술하고자 한다. powershell은 .NET과 연동되어 docx, xlsx 등의 Microsoft 운영
-체제가 지원하는 COM Object에 대한 API 가 다른 언어와 비교하여 우수하며 성능과 안정성이 보장되어 있다. 이러한 장점을 기반으로
-여러 파일 포맷에 대한 고려 시간을 최소화하고 수정이 용이한 powershell을 선택하였음에 합당한 선택이라고 생각한다. 
-
-powershell을 활용하여 검색 표준에 정규표현식을 도입하려고 하였으나, COM Object의 API상에 정규표현식을 이용한 검색은 제한이
-존재하는 것을 확인하였다. 이보다는 확장된 검색 기능을 활용하여 검색을 하는 것이 목적과 효율에 부합한다고 판단하였다. (와일드
-카드를 사용한 검색) 우선 이 방식은 마이크로소프트에서 제공하는 문서 객체에 한하여 적용가능하고 나머지 파일에 대해서는 정규
-표현식을 사용하는 것으로 우선 마무리한다. 
-
-powershell 스크립트의 구체적인 구성 내용
-해당 스크립트의 목적은 과탐이 발생하더라도 미탐이 발생하지 않는다는 것을 대전제로 한다. 미탐보다는 과탐이 낮고 과탐을 임의의
-방식을 통해 정정하는거나 추후의 탐지룰의 교정에 활용하는 것이 더욱더 낮다고 여겨진다. 이를 위해서는 탐지 문자열 또한 수집
-하는 것이 옳다. 수집된 문자열이 어디에서 기인하였는지를 추적하는 방법은 해당 PC 자산의 정보를 수집하는 것이다. 이에 대해서는 
-점검 대상의 소유주의 동의가 필요하므로 이에 대한 동의를 수집한다. 
-
-주민등록번호 추출 키워드 2016.12.17 여러번의 테스트를 통해 얻어낸 값이다. 
-[0-9]{6,6}[^t -]*[0-9]{7,7}
-
-#>
 [cmdletBinding()]
 Param(
  $Path = "D:\powershell-project\powershell-repo\SearchPersonalInformation"
@@ -98,12 +67,7 @@ ForEach ( $excel in $excelDocs ) {
         $range = $worksheet.usedrange
         ForEach ($row in $range.rows) {
             #"($($row.Rows.Row), $($row.Columns.Column))"
-            ForEach ($col in $row.Columns){
-                $row.Row
-                $col.Column
-                $col.Value2
-            }
-            #$row.Columns.Value2
+            $row.Columns.Value2 | Where-Object { $_ -match $searchText } | ForEach-Object { $_, $_.Range }
         }   
     }
     $workbook.close($false)
